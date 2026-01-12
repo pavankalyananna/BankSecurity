4ef79e366554f8a2baf86727e30fc230bb6049da
pavankalyananna <apk.pavan.07@gmail.com>
2026-01-12 23:21:33 +0530

    Add files via upload

---
+++ src/main/java/com/bank/config/BankUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.bank.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.bank.model.Customer;
+import com.bank.repository.CustomerRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BankUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+    
+    
+
+    public BankUserDetailsService(CustomerRepository customerRepository) {
+		super();
+		this.customerRepository = customerRepository;
+	}
+
+
+
+	@Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new
+                UsernameNotFoundException("User details not found for the user: " + username));
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+}
+
---
+++ src/main/java/com/bank/model/Customer.java
@@ -0,0 +1,47 @@
+package com.bank.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter @Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String pwd;
+    @Column(name = "role")
+    private String role;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+    
+    
+
+
+}
